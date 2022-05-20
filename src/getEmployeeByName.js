8c55d327407f8d2b169c84832a7df28f1f7484fb
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-05-20 15:49:18 -0300

    Requisito 3 - getEmployeeByName realizado

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const colaborador = data.employees.find((funcionario) => funcionario.firstName ===
+    employeeName || funcionario.lastName === employeeName);
+  return colaborador !== undefined ? colaborador : {};
 }
 
 module.exports = getEmployeeByName;
