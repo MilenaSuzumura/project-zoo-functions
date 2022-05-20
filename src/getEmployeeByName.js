5d26e13a9a185302b5f764c503226cedb55688d2
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-05-20 15:50:07 -0300

    requisitos do lint arrumados

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,8 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  const colaborador = data.employees.find((funcionario) => funcionario.firstName ===
-    employeeName || funcionario.lastName === employeeName);
+  const colaborador = data.employees.find((funcionario) =>
+    funcionario.firstName === employeeName || funcionario.lastName === employeeName);
   return colaborador !== undefined ? colaborador : {};
 }
 
