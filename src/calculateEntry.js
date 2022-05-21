34007838713e80fd96619168eff4e672d62b5c1d
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-05-21 15:59:33 -0300

    Requisito 8 - calculateEntry Realizado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const entradas = {'adult': 0, 'child': 0, 'senior': 0};
+  const criancas = entrants.filter((pessoas) => pessoas.age < 18);
+  const adultos = entrants.filter((pessoas) => pessoas.age >= 18 && pessoas.age < 50);
+  const idosos = entrants.filter((pessoas) => pessoas.age >= 50);
+  entradas.adult = adultos.length;
+  entradas.child = criancas.length;
+  entradas.senior = idosos.length;
+  return entradas;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined  || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const quantidade = countEntrants(entrants);
+  const precoAdulto = data.prices.adult * quantidade.adult;
+  const precoCrianca = data.prices.child * quantidade.child;
+  const precoIdoso = data.prices.senior * quantidade.senior;
+  return precoAdulto + precoCrianca + precoIdoso;
 }
 
 module.exports = { calculateEntry, countEntrants };
