20e4a5ad7217db46068a38cbcda00aa0c45f0b13
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-05-21 15:11:07 -0300

    Requisito 5 - countAnimals Realizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const existeValor = animal !== undefined ? animal : false;
+  const { specie, sex } = existeValor;
+  const semValor = data.species.reduce((acc, animais) => {
+    acc[animais.name] = animais.residents.length;
+    return acc;
+  }, {});
+  if (existeValor !== false) {
+    const procuraAnimal = data.species.filter((animais) => animais.name === specie)[0]
+      .residents.map((animais) => animais);
+    if (sex !== undefined) {
+      return procuraAnimal.filter((residentes) => residentes.sex === sex).length;
+    }
+    return procuraAnimal.length;
+  }
+  return semValor;
 }
 
 module.exports = countAnimals;
