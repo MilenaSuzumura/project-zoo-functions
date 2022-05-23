a1c763bfeb02fcab61ef5c83f92daf5a1daf878b
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-05-23 17:42:36 -0300

    Requisito 14 - getAnimalMap realizado

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
-  // seu código aqui
+  const valorExiste = options === undefined || Object.keys(options)[0] !== 'includeNames'
+    ? false : options;
+  const { sex, sorted } = valorExiste;
+  const comSex = data.species.reduce((acc, animal) => {
+    const objeto = {};
+    const sexAnimal = animal.residents.filter((residente) => residente.sex === sex)
+      .map((residente) => residente.name);
+    const sexExiste = sex !== undefined ? sexAnimal : animal.residents.map((residente) =>
+      residente.name);
+    const sortExiste = sorted === true ? sexExiste.sort() : sexExiste;
+    objeto[animal.name] = sortExiste;
+    acc.push(objeto);
+    return acc;
+  }, []);
+  return data.species.reduce((acc, animal, index) => {
+    const teste = valorExiste === false ? animal.name : comSex[index];
+    if (Object.keys(acc).includes(animal.location)) acc[animal.location].push(teste);
+    else acc[animal.location] = [teste];
+    return acc;
+  }, {});
 }
 
 module.exports = getAnimalMap;
