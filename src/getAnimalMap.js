1551d3fd54dde9ef7d9cfefb1fc9f1f5507b094a
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-05-24 17:52:06 -0300

    arrumei alguns nomes no getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,6 +1,26 @@
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
+  const valorExiste = options === undefined || Object.keys(options)[0] !== 'includeNames'
+    ? false : options;
+  const { sex, sorted } = valorExiste;
+  const comSex = data.species.reduce((acc, animal) => {
+    const objeto = {};
+    const sexAnimal = animal.residents.filter((residente) => residente.sex === sex)
+      .map((residente) => residente.name);
+    const sexExiste = sex !== undefined ? sexAnimal : animal.residents.map((residente) =>
+      residente.name);
+    const sortExiste = sorted === true ? sexExiste.sort() : sexExiste;
+    objeto[animal.name] = sortExiste;
+    acc.push(objeto);
+    return acc;
+  }, []);
+  return data.species.reduce((acc, animal, index) => {
+    const resultado = valorExiste === false ? animal.name : comSex[index];
+    if (Object.keys(acc).includes(animal.location)) acc[animal.location].push(resultado);
+    else acc[animal.location] = [resultado];
+    return acc;
+  }, {});
 }
 
 module.exports = getAnimalMap;
