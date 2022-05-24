1e3e76d3122eb8884d8c1bd1a16d057b46d0157e
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-05-24 16:08:13 -0300

    Requisito 10 - getOldestFromFirstSpecies realizado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const responsavel = data.employees.filter((funcionario) => funcionario.id === id)
+    .map(({ responsibleFor }) => responsibleFor);
+  const animais = responsavel[0].map((ids) => data.species.filter((animal) =>
+    animal.id === ids));
+  const filtroAnimal = animais[0].map((animal) => animal.residents
+    .reduce((acc, valorAtual) => {
+      let maisVelho = acc;
+      if (maisVelho.age < valorAtual.age) maisVelho = valorAtual;
+      return maisVelho;
+    }))[0];
+  return Object.values(filtroAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
