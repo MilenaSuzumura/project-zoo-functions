d2bdefaca04dea247f2e55fd2766c205d62d7689
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-05-25 15:41:04 -0300

    arrumei os pedidos do lint

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,14 +1,14 @@
 const handlerElephants = require('../src/handlerElephants');
 
-//Referencia: utilizei esse link para saber como procurar um valor do array https://jestjs.io/pt-BR/docs/using-matchers
+// Referencia: utilizei esse link para saber como procurar um valor do array https://jestjs.io/pt-BR/docs/using-matchers
 describe('Testes da função HandlerElephants', () => {
   test('Para o argumento count, retorna a quantidade de elefantes que é 4', () => {
     expect(handlerElephants('count')).toBe(4);
   });
-  test('Para o argumento names, retorna um array de nomes que possui o nome Jefferson', () =>{
+  test('Para o argumento names, retorna um array de nomes que possui o nome Jefferson', () => {
     expect(handlerElephants('names')).toContain('Jefferson');
   });
   test('Para o argumento averageAge, retorna um número próximo a 10.5', () => {
     expect(handlerElephants('averageAge')).toBe(10.5);
-  })
+  });
 });
