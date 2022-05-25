3a4e512e164f2ca56b692079c2a8866d8e49cd6e
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-05-25 15:51:29 -0300

    7 - handlerElephants. Fiz os outros testes

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -11,4 +11,16 @@ describe('Testes da função HandlerElephants', () => {
   test('Para o argumento averageAge, retorna um número próximo a 10.5', () => {
     expect(handlerElephants('averageAge')).toBe(10.5);
   });
+  test('Para o argumento location, retorna a string NW', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  test('Para o argumento popularity, retorna um número igual ou maior a 5', () => {
+    expect(handlerElephants('popularity')).toBeGreaterThanOrEqual(5);
+  });
+  test('Para o argumento availability, retorna um array de dias da semana que não contém Monday', () => {
+    expect(handlerElephants('availability')).not.toContain('Monday');
+  });
+  test('Não passando argumentos a função deve retornar undefined', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
 });
