0fa9bfc4974cf538e2d3bd62b19c8ec9dd43c372
Milena Suzumura <milena.suzumura11@gmail.com>
2023-03-21 18:35:51 -0300

    feat: create mock employees

---
+++ test/mocks/employees.mock.js
@@ -0,0 +1,14 @@
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+
+const stephanie = {
+  id: stephanieId,
+  firstName: 'Stephanie',
+  lastName: 'Strauss',
+  managers: [],
+  responsibleFor: [
+    '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae',
+    '01422318-ca2d-46b8-b66c-3e9e188244ed',
+  ],
+};
+
+module.exports = { stephanie, stephanieId };
