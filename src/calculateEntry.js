b2c326bed8d55436945928ce1ea56041c5a08317
Milena Suzumura <milena.suzumura11@gmail.com>
2023-03-20 21:47:07 -0300

    feat: criei a função para calcular a quantidade de crianças

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,5 +1,10 @@
 const data = require('../data/zoo_data');
 
+function countChild(entrants) {
+  const children = entrants.filter((person) => person.age < 18);
+  return children.length;
+}
+
 function countEntrants(entrants) {
   // seu código aqui
 }
