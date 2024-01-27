675dba97ccfcc493bd77779b37b4673849d8792e
Milena Suzumura <milena.suzumura11@gmail.com>
2024-01-27 20:55:40 -0300

    feat: seeders de Hours terminado

--- app/backend/src/seeders/20240127232701-demo-hours.js
+++ app/backend/src/seeders/20240127232701-demo-hours.js
@@ -3,23 +3,18 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('Hours', [
+      { id: 1,  day: 'Monday', open: 0, close: 0, createdAt: new Date(), updatedAt: new Date() },
+      { id: 2,  day: 'Tuesday', open: 8, close: 6, createdAt: new Date(), updatedAt: new Date() },
+      { id: 3,  day: 'Wednesday', open: 8, close: 6, createdAt: new Date(), updatedAt: new Date() },
+      { id: 4,  day: 'Thursday', open: 10, close: 8, createdAt: new Date(), updatedAt: new Date() },
+      { id: 5,  day: 'Friday', open: 10, close: 8, createdAt: new Date(), updatedAt: new Date() },
+      { id: 6,  day: 'Saturday', open: 8, close: 10, createdAt: new Date(), updatedAt: new Date() },
+      { id: 7,  day: 'Sunday', open: 8, close: 8, createdAt: new Date(), updatedAt: new Date() }
+    ], { timestamps: false })
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Hours', null, {});
   }
 };
